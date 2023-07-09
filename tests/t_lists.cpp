f9b43cafe23796e230a6ffade068405d11800aaa
Daniil <post@windhelm.ru>
2023-07-09 23:44:51 +0300

    Добавлены тесты для конструкторов

--- tests/t_lists.cpp
+++ tests/t_lists.cpp
@@ -14,3 +14,16 @@ TEST(test_list, default_constuctor) {
 
   EXPECT_EQ(sizeof(test), sizeof(test_std));
 }
+
+TEST(test_list, constuctor_count_value) {
+  s21::list<int> test(11, 567);
+  std::list<int> test_std(11, 567);
+
+  EXPECT_EQ(false, test.empty());
+  EXPECT_EQ(test_std.empty(), test.empty());
+
+  EXPECT_EQ(11, test.size());
+  EXPECT_EQ(test_std.size(), test.size());
+
+  EXPECT_EQ(sizeof(test), sizeof(test_std));
+}
