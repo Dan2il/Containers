1a8d2f74b5946b376338aaa23051169e78dc91b8
Daniil <post@windhelm.ru>
2023-07-10 23:57:16 +0300

    Добавлена реализация конструктора по интервалу

--- source/list/s21_list.h
+++ source/list/s21_list.h
@@ -9,9 +9,10 @@ template <typename Type>
 struct Node {
   Type data;
   Node* next_node;
+  Node* previous_node;
   long int num_node;
 
-  Node() : next_node(nullptr) { num_node = -1; }
+  Node() : next_node(nullptr), previous_node(nullptr) { num_node = -1; }
   Node(const Type& t) : Node() { data = t; };
 };
 
@@ -20,10 +21,10 @@ class list {
  public:
   explicit list() = default;
   list(const size_t count, const Type& value);
-  // explicit list(size_type count);
+  explicit list(size_t count);
 
-  //   template <class InputIt>
-  //   list(InputIt first, InputIt last);
+  template <class InputIt>
+  list(InputIt first, InputIt last);
 
   //   list(const list& other);
   //   list(list&& other);
@@ -39,6 +40,8 @@ class list {
   Alloc alloc_;
   Node<Type>* null_node_ = nullptr;
   size_t stored_ = 0;
+
+  void LinkPointer(Node<Type>* for_link_next, Node<Type>* for_link_previous);
 };
 
 template <typename Type, typename Alloc>
@@ -47,13 +50,42 @@ list<Type, Alloc>::list(const size_t count, const Type& value) {
   alloc_.construct(null_node_, value);
 
   Node<Type>* buffer_address = null_node_;
-  for (size_t counter = 0; counter < count; ++counter) {
-    Node<Type>* push = nullptr;
-    push = alloc_.allocate(1);
-    alloc_.construct(push, value);
-    buffer_address->next_node = push;
-    buffer_address = push;
-    ++stored_;
+  for (; stored_ < count; ++stored_) {
+    buffer_address->next_node = alloc_.allocate(1);
+    alloc_.construct(buffer_address->next_node, value);
+    LinkPointer(buffer_address, buffer_address->next_node);
+    buffer_address = buffer_address->next_node;
+    null_node_->previous_node = buffer_address;
+  }
+}
+
+template <typename Type, typename Alloc>
+list<Type, Alloc>::list(size_t count) {
+  null_node_ = alloc_.allocate(1);
+  alloc_.construct(null_node_);
+  Node<Type>* buffer_address = null_node_;
+  for (; stored_ < count; ++stored_) {
+    buffer_address->next_node = alloc_.allocate(1);
+    alloc_.construct(buffer_address->next_node);
+    LinkPointer(buffer_address, buffer_address->next_node);
+    buffer_address = buffer_address->next_node;
+    null_node_->previous_node = buffer_address;
+  }
+}
+
+template <typename Type, typename Alloc>
+template <class InputIt>
+s21::list<Type, Alloc>::list(InputIt first, InputIt last)
+    : null_node_(alloc_.allocate(1)) {
+  // null_node_ = alloc_.allocate(1);
+  Node<Type>* buffer_address = null_node_;
+  alloc_.construct(null_node_);
+  for (; first != last; first++) {
+    buffer_address->next_node = alloc_.allocate(1);
+    alloc_.construct(buffer_address->next_node, *first);
+    LinkPointer(buffer_address, buffer_address->next_node);
+    buffer_address = buffer_address->next_node;
+    null_node_->previous_node = buffer_address;
   }
 }
 
@@ -77,6 +109,13 @@ bool s21::list<Type, Alloc>::empty() const noexcept {
 template <typename Type, typename Alloc>
 size_t s21::list<Type, Alloc>::size() const noexcept {
   return stored_;
+}
+
+template <typename Type, typename Alloc>
+void s21::list<Type, Alloc>::LinkPointer(Node<Type>* for_link_next,
+                                         Node<Type>* for_link_previous) {
+  for_link_next->next_node = for_link_previous;
+  for_link_previous->previous_node = for_link_next;
 }
 
 }  // namespace s21
--- tests/t_lists.cpp
+++ tests/t_lists.cpp
@@ -26,4 +26,36 @@ TEST(test_list, constuctor_count_value) {
   EXPECT_EQ(test_std.size(), test.size());
 
   EXPECT_EQ(sizeof(test), sizeof(test_std));
+
+  // добавить проверку циклом
+}
+
+TEST(test_list, constuctor_count) {
+  s21::list<int> test(11);
+  std::list<int> test_std(11);
+
+  EXPECT_EQ(false, test.empty());
+  EXPECT_EQ(test_std.empty(), test.empty());
+
+  EXPECT_EQ(11, test.size());
+  EXPECT_EQ(test_std.size(), test.size());
+
+  EXPECT_EQ(sizeof(test), sizeof(test_std));
+
+  // добавить проверку циклом
+}
+
+TEST(test_list, constructor_range) {
+  // std::vector<int> t = {1, 1, 11, 234, 12412321, 23};
+
+  // s21::list<int> test(t.begin(), t.end());
+  // std::list<int> test_std(t.begin(), t.end());
+
+  // EXPECT_EQ(false, test.empty());
+  // EXPECT_EQ(test_std.empty(), test.empty());
+
+  // EXPECT_EQ(11, test.size());
+  // EXPECT_EQ(test_std.size(), test.size());
+
+  // EXPECT_EQ(sizeof(test), sizeof(test_std));
 }
