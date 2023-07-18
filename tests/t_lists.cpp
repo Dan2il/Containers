ce1b0255b4e329bc8b2306df7d3a3ee7e72a7f35
Daniil <post@windhelm.ru>
2023-07-18 13:36:19 +0300

    Добавлены конструкторы

--- tests/t_lists.cpp
+++ tests/t_lists.cpp
@@ -45,17 +45,67 @@ TEST(test_list, constuctor_count) {
   // добавить проверку циклом
 }
 
-TEST(test_list, constructor_range) {
-  // std::vector<int> t = {1, 1, 11, 234, 12412321, 23};
+TEST(test_list, constructor_initializer_list) {
+  s21::list<double> test{1, 23, 53223, 543, 134234, 54, 56.6542};
+  std::list<double> test_std{1, 23, 53223, 543, 134234, 54, 56.6542};
 
-  // s21::list<int> test(t.begin(), t.end());
-  // std::list<int> test_std(t.begin(), t.end());
+  EXPECT_EQ(false, test.empty());
+  EXPECT_EQ(test_std.empty(), test.empty());
 
-  // EXPECT_EQ(false, test.empty());
-  // EXPECT_EQ(test_std.empty(), test.empty());
+  EXPECT_EQ(7, test.size());
+  EXPECT_EQ(test_std.size(), test.size());
 
-  // EXPECT_EQ(11, test.size());
-  // EXPECT_EQ(test_std.size(), test.size());
+  EXPECT_EQ(sizeof(test), sizeof(test_std));
 
-  // EXPECT_EQ(sizeof(test), sizeof(test_std));
+  EXPECT_DOUBLE_EQ(1, test.front());
+  EXPECT_DOUBLE_EQ(56.6542, test.back());
+
+  EXPECT_DOUBLE_EQ(test_std.front(), test.front());
+  EXPECT_DOUBLE_EQ(test_std.back(), test.back());
+
+  // добавить проверку циклом
+}
+
+TEST(test_list, constructor_copy) {
+  s21::list<double> test{1, 23, 53223, 543, 134234, 54, 56.6542};
+  std::list<double> test_std{1, 23, 53223, 543, 134234, 54, 56.6542};
+
+  s21::list<double> test_copy(test);
+  std::list<double> test_std_copy(test_std);
+
+  EXPECT_EQ(false, test_copy.empty());
+  EXPECT_EQ(test_std_copy.empty(), test_copy.empty());
+
+  EXPECT_EQ(7, test_copy.size());
+  EXPECT_EQ(test_std_copy.size(), test_copy.size());
+
+  EXPECT_DOUBLE_EQ(1, test_copy.front());
+  EXPECT_DOUBLE_EQ(56.6542, test_copy.back());
+
+  EXPECT_DOUBLE_EQ(test_std_copy.front(), test_copy.front());
+  EXPECT_DOUBLE_EQ(test_std_copy.back(), test_copy.back());
+
+  // добавить проверку циклом
+}
+
+TEST(test_list, constructor_move) {
+  s21::list<double> test{1, 23, 53223, 543, 134234, 54, 56.6542};
+  std::list<double> test_std{1, 23, 53223, 543, 134234, 54, 56.6542};
+
+  s21::list<double> test_move(std::move(test));
+  std::list<double> test_std_move(std::move(test_std));
+
+  // EXPECT_EQ(false, test_move.empty());
+  // EXPECT_EQ(test_std_move.empty(), test_move.empty());
+
+  // EXPECT_EQ(7, test_move.size());
+  // EXPECT_EQ(test_std_move.size(), test_move.size());
+
+  // EXPECT_DOUBLE_EQ(1, test_move.front());
+  // EXPECT_DOUBLE_EQ(56.6542, test_move.back());
+
+  // EXPECT_DOUBLE_EQ(test_std_move.front(), test_move.front());
+  // EXPECT_DOUBLE_EQ(test_std_move.back(), test_move.back());
+
+  // добавить проверку циклом
 }
