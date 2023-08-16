a3a3ec70605ac2623e8b1eb7ebf1899bdcf08f40
Windhelm Melessa <windhelm@il-j6.msk.21-school.ru>
2023-08-16 19:26:26 +0300

    Переписан метод sort

--- source/list/s21_list.h
+++ source/list/s21_list.h
@@ -119,6 +119,7 @@ class list {
   Node<Type>* CreateTree();
   void PushNodeTree(Node<Type>* push, Node<Type>* head);
   void GetDataTree(Node<Type>* head);
+  void PrintTree(Node<Type>* head);
 };
 
 template <typename Type>
@@ -403,75 +404,103 @@ template <typename Type>
 void s21::list<Type>::sort() {
   if (stored_ > 1) {
     Node<Type>* head = CreateTree();
+    // PrintTree(head);
     GetDataTree(head);
   }
-  end_node_->next_node = nullptr;
-  end_node_->previous_node = nullptr;
-  stored_ = 0;
+  // end_node_->next_node = nullptr;
+  // end_node_->previous_node = nullptr;
+  // stored_ = 0;
 }
 
 template <typename Type>
 Node<Type>* s21::list<Type>::CreateTree() {
   Node<Type>* head = end_node_->next_node;
+  Node<Type>* step = head->next_node;
 
-  for (Node<Type>* step = head; step != end_node_;) {
-    Node<Type>* push = step;
-    step = step->next_node;
+  end_node_->next_node = nullptr;
+  end_node_->previous_node = nullptr;
 
-    push->next_node = nullptr;
-    push->previous_node = nullptr;
+  head->next_node = nullptr;
+  head->previous_node = nullptr;
 
-    PushNodeTree(push, head);
-    if (step->next_node == end_node_) {
-      break;
+  stored_ = 0;
+
+  // std::cout << "stored_ = 0;" << std::endl;
+
+  for (; step != end_node_;) {
+    Node<Type>* buf_next_step = step->next_node;
+
+    step->next_node = nullptr;
+    step->previous_node = nullptr;
+    // std::cout << "for " << step->data << std::endl;
+
+    for (Node<Type>* step_of_head = head;;) {
+      assert(step_of_head != nullptr);
+      if (step->data < step_of_head->data) {
+        if (step_of_head->next_node != nullptr) {
+          step_of_head = step_of_head->next_node;
+        } else {
+          step_of_head->next_node = step;
+          break;
+        }
+      } else {
+        if (step_of_head->previous_node != nullptr) {
+          step_of_head = step_of_head->previous_node;
+        } else {
+          // std::cout << "step_of_head->previous_node = step;" << std::endl;
+          step_of_head->previous_node = step;
+          break;
+        }
+      }
     }
+    step = buf_next_step;
   }
+  // std::cout << "for end" << std::endl;
   return head;
 }
 
 template <typename Type>
-void s21::list<Type>::PushNodeTree(Node<Type>* push, Node<Type>* head) {
-  if (push->data < head->data) {
-    if (head->next_node != nullptr) {
-      PushNodeTree(push, head->next_node);
-    } else {
-      head->next_node = push;
-    }
-  } else {
-    if (head->previous_node != nullptr) {
-      PushNodeTree(push, head->previous_node);
-    } else {
-      head->previous_node = push;
-    }
+void s21::list<Type>::PrintTree(Node<Type>* head) {
+  assert(head != nullptr);
+  if (head->next_node) {
+    GetDataTree(head->next_node);
+  }
+
+  // std::cout << "data == " << head->data << std::endl;
+
+  if (head->previous_node) {
+    GetDataTree(head->previous_node);
   }
 }
 
 template <typename Type>
 void s21::list<Type>::GetDataTree(Node<Type>* head) {
-  assert(head != nullptr);
-  if (head->next_node != nullptr) {
-    // GetDataTree(head->next_node);
+  if (head->next_node) {
+    GetDataTree(head->next_node);
   }
-  std::cout << head->data << std::endl;
-  // Node<Type>* link = head;
-  // if (!end_node_->previous_node) {
-  //   end_node_->next_node = link;
-  //   link->next_node = end_node_;
 
-  //   end_node_->previous_node = link;
-  //   link->previous_node = end_node_;
-  // } else {
-  //   Node<Type>* buf_prev_end = end_node_->previous_node;
+  Node<Type>* buf_prev_head = head->previous_node;
 
-  //   buf_prev_end->next_node = link;
-  //   link->next_node = end_node_;
+  if (!end_node_->next_node) {
+    end_node_->next_node = head;
+    end_node_->previous_node = head;
 
-  //   end_node_->previous_node = link;
-  //   link->previous_node = buf_prev_end;
-  // }
+    head->next_node = end_node_;
+    head->previous_node = end_node_;
+    ++stored_;
+  } else {
+    Node<Type>* buf_prev_list_node = end_node_->previous_node;
 
-  if (head->previous_node != nullptr) {
-    // GetDataTree(head->previous_node);
+    buf_prev_list_node->next_node = head;
+    head->next_node = end_node_;
+
+    end_node_->previous_node = head;
+    head->previous_node = buf_prev_head;
+    ++stored_;
+  }
+
+  if (buf_prev_head) {
+    GetDataTree(buf_prev_head);
   }
 }
 
