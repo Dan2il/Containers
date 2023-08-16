5f42e0b5f1eb4bb7df21dbf438e6ee2f137c7f62
Windhelm Melessa <windhelm@il-j6.msk.21-school.ru>
2023-08-16 21:01:38 +0300

    Декомпозиция

--- source/list/s21_list.h
+++ source/list/s21_list.h
@@ -116,10 +116,11 @@ class list {
                             pointer_node first, pointer_node last);
   void LinkPointerNode(Node<Type>* prev_node, Node<Type>* next_node);
 
+  void DelLink(Node<Type>* node);
+
   Node<Type>* CreateTree();
-  void PushNodeTree(Node<Type>* push, Node<Type>* head);
+  void PushNodeTree(Node<Type>* step, Node<Type>* head);
   void GetDataTree(Node<Type>* head);
-  void PrintTree(Node<Type>* head);
 };
 
 template <typename Type>
@@ -156,8 +157,6 @@ s21::list<Type>::list(const list<Type>& other) : list() {
 template <typename Type>
 s21::list<Type>::list(list&& other) noexcept {
   *this = std::move(other);
-  // end_node_ = std::exchange(other.end_node_, nullptr);
-  // stored_ = std::exchange(other.stored_, 0);
 }
 
 template <typename Type>
@@ -238,11 +237,11 @@ typename s21::list<Type>::iterator s21::list<Type>::insert(iterator pos,
   Node<Type>* new_node = CreateNode(value);
 
   if (!stored_) {
-    LinkPointerNode(end_node_, new_node);
-    LinkPointerNode(new_node, end_node_);
+    LinkPointerNodeRange(end_node_, end_node_, new_node, new_node);
+
   } else {
-    LinkPointerNode(pos.link_node_->previous_node, new_node);
-    LinkPointerNode(new_node, pos.link_node_);
+    LinkPointerNodeRange(pos.link_node_->previous_node, pos.link_node_,
+                         new_node, new_node);
   }
   ++stored_;
   return iterator(this, new_node);
@@ -252,11 +251,9 @@ template <typename Type>
 void s21::list<Type>::push_back(const Type& value) {
   Node<Type>* push = CreateNode(value);
   if (!stored_) {
-    LinkPointerNode(end_node_, push);
-    LinkPointerNode(push, end_node_);
+    LinkPointerNodeRange(end_node_, end_node_, push, push);
   } else {
-    LinkPointerNode(end_node_->previous_node, push);
-    LinkPointerNode(push, end_node_);
+    LinkPointerNodeRange(end_node_->previous_node, end_node_, push, push);
   }
   ++stored_;
 }
@@ -265,11 +262,9 @@ template <typename Type>
 void s21::list<Type>::push_front(const Type& value) {
   Node<Type>* push = CreateNode(value);
   if (!stored_) {
-    LinkPointerNode(end_node_, push);
-    LinkPointerNode(push, end_node_);
+    LinkPointerNodeRange(end_node_, end_node_, push, push);
   } else {
-    LinkPointerNode(push, end_node_->next_node);
-    LinkPointerNode(end_node_, push);
+    LinkPointerNodeRange(push, push, end_node_->next_node, end_node_);
   }
   ++stored_;
 }
@@ -281,8 +276,7 @@ void s21::list<Type>::pop_front() {
     if (stored_ > 1) {
       LinkPointerNode(end_node_, del_node->next_node);
     } else {
-      end_node_->next_node = nullptr;
-      end_node_->previous_node = nullptr;
+      DelLink(end_node_);
     }
     FreeNode(del_node);
     --stored_;
@@ -296,8 +290,7 @@ void s21::list<Type>::pop_back() {
     if (stored_ > 1) {
       LinkPointerNode(del_node->previous_node, end_node_);
     } else {
-      end_node_->next_node = nullptr;
-      end_node_->previous_node = nullptr;
+      DelLink(end_node_);
     }
     FreeNode(del_node);
     --stored_;
@@ -333,8 +326,7 @@ void s21::list<Type>::merge(list& other) {
           it_this = begin();
         }
         if (!other.stored_) {
-          other.end_node_->next_node = nullptr;
-          other.end_node_->previous_node = nullptr;
+          DelLink(other.end_node_);
           break;
         }
       }
@@ -361,8 +353,7 @@ void s21::list<Type>::splice(const_iterator pos, list& other) {
     }
     stored_ += other.stored_;
 
-    other.end_node_->next_node = nullptr;
-    other.end_node_->previous_node = nullptr;
+    DelLink(other.end_node_);
     other.stored_ = 0;
   }
 }
@@ -403,73 +394,44 @@ void s21::list<Type>::unique() {
 template <typename Type>
 void s21::list<Type>::sort() {
   if (stored_ > 1) {
-    Node<Type>* head = CreateTree();
-    // PrintTree(head);
-    GetDataTree(head);
+    GetDataTree(CreateTree());
   }
-  // end_node_->next_node = nullptr;
-  // end_node_->previous_node = nullptr;
-  // stored_ = 0;
 }
 
 template <typename Type>
 Node<Type>* s21::list<Type>::CreateTree() {
   Node<Type>* head = end_node_->next_node;
   Node<Type>* step = head->next_node;
-
-  end_node_->next_node = nullptr;
-  end_node_->previous_node = nullptr;
-
-  head->next_node = nullptr;
-  head->previous_node = nullptr;
-
-  stored_ = 0;
-
-  // std::cout << "stored_ = 0;" << std::endl;
-
+  DelLink(end_node_);
+  DelLink(head);
   for (; step != end_node_;) {
     Node<Type>* buf_next_step = step->next_node;
-
     step->next_node = nullptr;
     step->previous_node = nullptr;
-    // std::cout << "for " << step->data << std::endl;
-
-    for (Node<Type>* step_of_head = head;;) {
-      assert(step_of_head != nullptr);
-      if (step->data < step_of_head->data) {
-        if (step_of_head->next_node != nullptr) {
-          step_of_head = step_of_head->next_node;
-        } else {
-          step_of_head->next_node = step;
-          break;
-        }
-      } else {
-        if (step_of_head->previous_node != nullptr) {
-          step_of_head = step_of_head->previous_node;
-        } else {
-          // std::cout << "step_of_head->previous_node = step;" << std::endl;
-          step_of_head->previous_node = step;
-          break;
-        }
-      }
-    }
+    PushNodeTree(step, head);
     step = buf_next_step;
   }
-  // std::cout << "for end" << std::endl;
   return head;
 }
 
 template <typename Type>
-void s21::list<Type>::PrintTree(Node<Type>* head) {
-  assert(head != nullptr);
-  if (head->next_node) {
-    GetDataTree(head->next_node);
-  }
-
-  // std::cout << "data == " << head->data << std::endl;
-
-  if (head->previous_node) {
-    GetDataTree(head->previous_node);
+void s21::list<Type>::PushNodeTree(Node<Type>* step, Node<Type>* head) {
+  for (Node<Type>* step_of_head = head;;) {
+    if (step->data < step_of_head->data) {
+      if (step_of_head->next_node != nullptr) {
+        step_of_head = step_of_head->next_node;
+      } else {
+        step_of_head->next_node = step;
+        break;
+      }
+    } else {
+      if (step_of_head->previous_node != nullptr) {
+        step_of_head = step_of_head->previous_node;
+      } else {
+        step_of_head->previous_node = step;
+        break;
+      }
+    }
   }
 }
 
@@ -482,21 +444,9 @@ void s21::list<Type>::GetDataTree(Node<Type>* head) {
   Node<Type>* buf_prev_head = head->previous_node;
 
   if (!end_node_->next_node) {
-    end_node_->next_node = head;
-    end_node_->previous_node = head;
-
-    head->next_node = end_node_;
-    head->previous_node = end_node_;
-    ++stored_;
+    LinkPointerNodeRange(end_node_, end_node_, head, head);
   } else {
-    Node<Type>* buf_prev_list_node = end_node_->previous_node;
-
-    buf_prev_list_node->next_node = head;
-    head->next_node = end_node_;
-
-    end_node_->previous_node = head;
-    head->previous_node = buf_prev_head;
-    ++stored_;
+    LinkPointerNodeRange(end_node_->previous_node, end_node_, head, head);
   }
 
   if (buf_prev_head) {
@@ -581,11 +531,8 @@ template <typename Type>
 void s21::list<Type>::LinkPointerNodeRange(pointer_node prev_pos,
                                            pointer_node pos, pointer_node first,
                                            pointer_node last) {
-  prev_pos->next_node = first;
-  last->next_node = pos;
-
-  pos->previous_node = last;
-  first->previous_node = prev_pos;
+  LinkPointerNode(prev_pos, first);
+  LinkPointerNode(last, pos);
 }
 
 template <typename Type>
@@ -593,6 +540,12 @@ void s21::list<Type>::LinkPointerNode(Node<Type>* prev_node,
                                       Node<Type>* next_node) {
   prev_node->next_node = next_node;
   next_node->previous_node = prev_node;
+}
+
+template <typename Type>
+void s21::list<Type>::DelLink(Node<Type>* node) {
+  node->next_node = nullptr;
+  node->previous_node = nullptr;
 }
 
 // Iterators
